10451fe00cc42b06b3c6f8c742f85f0b5e4595ca
Palash Sharma <96536529+TechbyPanda@users.noreply.github.com>
2026-09-24 16:20:43 +0000

    data controller and target

--- app/javascript/controllers/adjuster_controller.js
+++ app/javascript/controllers/adjuster_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="adjuster"
 export default class extends Controller {
+  static targets = ["quantity"]
   connect() {
     console.log("Hello from adjuster controller")
   }
 
   add() {
-    console.log("Add button clicked")
+    const current = parseInt(this.quantityTarget.textContent)
+    this.quantityTarget.textContent = `${current + 5} units`
   }
 }
